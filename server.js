c7825d4cbe0e200a7e51b7fe59af986f4ed92132
Dan Higham <dhigham@rbcon.com>
2012-08-25 21:27:41 +0100

    Added recursive file search

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
-var express = require('express');
+var express = require('express')
+  , glob = require('glob');
+
 var app = express();
+var files_info = new Array();
+
+searchFiles('/Users/danhigham/Music');
 
 app.get('/', function(req, res){
   res.send('hello world');
 });
+
+function searchFiles(cwd) {
+
+  options = {'cwd': cwd}
+  glob("**/*.{mp3,mp4,m4a}", options, function (er, files) {
+
+
+    console.log(files);
+
+  });
+}
+
+
 
 app.listen(3000);
