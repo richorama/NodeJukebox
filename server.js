b070ee1e427cdf12d90008c3ee49f956723cc3a1
Richard Astbury <Richard.Astbury@gmail.com>
2012-08-25 21:24:17 +0100

    added taglist mp3 tag reader

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
+var tracklist = require("tracklist");
+
+tracklist.list(".", function (err, result) {
+	console.log(results);
+});
+
 app.get('/', function(req, res){
-  res.send('hello world');
+	res.send('hello world');
 });
 
 app.listen(3000);
